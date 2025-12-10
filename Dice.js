ace43cf858c96b077215952737ddad38a3375830
Alexa828 <alexvalenrodriguez@gmail.com>
2025-12-10 01:21:34 -0700

    Add files via upload

--- Dice.js
+++ Dice.js
@@ -170,3 +170,47 @@ function rollDice(){
         //get a random number between 0 and 799
         return Math.floor(Math.random() * 800);
         }
+
+
+// function to test for the palindrome
+        function testPalin(){
+            // get the value that the user enetered in the textbox & remove any space at the beginning or end of the string
+            let userInput = document.getElementById("txtInput").value.trim();
+
+           console.log("userInput=" + userInput);
+          
+            // this statement implies == true, you could write it out
+            // isPalindrome(userInput) == true
+            if (isPalindrome(userInput)){
+                document.getElementById("divResult").textContent = "Yes, " + userInput + " is a palindrome."
+            }
+                 else{
+                    document.getElementById("divResult").textContent = "No, " + userInput + " is not a palindrome."
+                 }
+       }
+
+        // function will test to see if a word is a palindrome
+        function isPalindrome(wordToTest){
+            // remove any spaces inside of the word(s)
+            let cleanedWord = wordToTest.replace(/\s/g, "");
+
+            // change any UPPER CASE letters to lower case
+            cleanedWord = cleanedWord.toLowerCase();
+
+            console.log("cleanedWord=" + cleanedWord);
+            
+            // convert the cleaned string to an array
+            let arrCleaned = cleanedWord.split("");
+
+            // reverse the array content
+            arrCleaned = arrCleaned.reverse();
+
+            // take our reversed array and convert it back to a string
+            let reversedWord = arrCleaned.join("");
+
+            console.log("reversedWord=" + reversedWord);
+
+            // compare if the cleaned and reversed words are the same
+            return cleanedWord == reversedWord;
+        }
+
